5ff7082a0723d6677527e651a44fe57265a7b668
SheriffKoder <sheriff.koder@hotmail.com>
2023-05-21 18:41:34 +0300

    practice1: work on js pseudo code, html/css layout

---
+++ recap/practice/practice1/practice1.js
@@ -0,0 +1,203 @@
+/*
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////points to recap on
+create new object
+with multi keys
+arrow function key that declares a variable
+this function is a constructor
+two word keys
+
+
+console.log
+
+function that has this.variable
+call that sum outside
+strict mode
+
+switch case=
+give case input, case this input change something=
+
+
+
+access the object keys from outside=
+update using the assign method=
+based on the received word+ "wordkey"=
+
+pass to a key-function an argument=
+give that constructor a custom prototype method=
+make a new instance of the constructor and use the method on=
+give parameters using spread args=
+
+merge the object with another object using the spread syntax=
+copy an array into another array format with concatenate using spread=
+//merging objects separate using the spread syntax
+
+
+output the keys of the object=
+output each key separately using for let with checking if it is it's property=
+
+use map to change an array into a new array=
+copy this array using slice or spread=
+
+convert that object into json
+
+
+(1) design the script
+(2) design the layout kp
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////points organized to build a pseudo code on
+create a new object
+with multi keys
+"arrow constructor" function key that declares variables named with two words
+
+
+give the "arrow constructor" custom prototype methods=
+that assign new keys to the object using assign=
+or merge this object with another object using spread=
+or merge a key-array in between another array using a provided location after knowing the array length (employees)=
+or output the keys of the object (an array) and store into a keys-key
+or output each key separately=
+
+make new instances of the "arrow constructor"=
+where you pass parameters using spread=
+
+copy the employees array to another new object's key-array using slice or spread
+use map to increase salaries key=
+
+
+switch case that adds name to access the needed custom method
+
+merge all departments objects (combined)=
+
+
+
+make it intuitive
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+//////application pseudo code
+
+let us say you have 2 departments stored
+
+let departmentOne = {
+
+    //array of 3x single-employee objects
+    levelOne: [ {name: , age: , position; salary: }],
+    levelTwo: [ {name: , age: , position; salary: }],
+
+    addaNewLevelfunction
+
+
+}
+
+let departmentTwo = {
+
+    //array of 3x single-employee objects
+    levelOne: [ {name: , age: , position; salary: }]
+    levelTwo: [ {name: , age: , position; salary: }]
+
+
+}
+
+//employee constructor, 
+custom method to view details using for let/in
+custom method to add a detail with a value using defineProperty
+increaseSalaryOfAnEmployee: function () {
+    //this will take an employee and update his salary index
+},
+changePositionOfAnEmployee: function () {
+    //this will access the employee's position and change it by the input requested
+}
+changeLocationOfAnEmployee:
+    //this will display the departments and ask for a department
+    //this will display the levels and ask for a level
+    //this will display the desks(array index) and ask for a desk(array index)
+    //this will remove the employee from the previous location
+
+removeEmployee (same as) changeLocationOfAnEmployee:
+    //x this will display the departments and ask for a department
+    //x this will display the levels and ask for a level
+    //x this will display the desks(array index) and ask for a desk(array index)
+    // this will remove the employee from the previous location
+
+
+//also would need an action object
+
+let actionObject = {
+
+    mergeAlldepartmentsObjectsSeparateKeys: function () {
+        //combine department objects using spread ...arg
+    },
+
+    mergeAlldepartmentsObjectsCombined: function () {
+    //combine department objects using spread ...arg
+    },
+
+    increaseAllSalaries: 
+        //receive all salaries
+        //map to increase the salaries
+        //put back
+
+    displayAllDepartments
+        //using object.keys
+
+    addNewEmployee (same as) changeLocationOfAnEmployee:
+        //this will display the departments and ask for a department
+        //this will display the levels and ask for a level
+        //this will display the desks(array index) and ask for a desk(array index)
+        //x this will remove the employee from the previous location
+
+    displayAllEmployees
+        //
+
+    displayEmployee
+        //this will ask for the name/code provided from displayAllEmployees
+        //and provide info using the employee constructor method
+
+}
+
+
+//the layout
+//let us begin with a table of three cols
+//col1: buttons
+//col2: the console
+//col3: all employees within the departments
+
+//button to store the code/object database in a file using node.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
