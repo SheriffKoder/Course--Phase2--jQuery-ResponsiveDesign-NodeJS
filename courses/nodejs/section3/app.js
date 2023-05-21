13a65a7ab9b639198fb7d734958adf684638b095
SheriffKoder <sheriff.koder@hotmail.com>
2023-05-21 20:17:24 +0300

    NodeJS: page redirection and file creating with the user input provided

--- courses/nodejs/section3/app.js
+++ courses/nodejs/section3/app.js
@@ -29,7 +29,7 @@
 
 //can put a path to a local file starting with './path.js'
 // no ./ will use the global module even if there is a local one
-const http = require('http'); 
+//const http = require('http'); 
 
 //has two arguments
 //a request: incoming message
@@ -84,6 +84,7 @@ const server = http.createServer((req, res) => {
 
 
     //loop until a request happens then exit
+    //but we want the server not to exit
     //process.exit();
 });
 */
@@ -156,10 +157,17 @@ const server = http.createServer((req, res) => {
 /*
 */
 
+/*
+
+const http = require('http'); 
+
 
 const server = http.createServer((req, res) => {
 
     //console.log(req.url, req.method, req.headers);
+    //form with action directing to /message and method POST
+    //that is activated on the button of type submit
+    //that will send a new request
     const url = req.url;
     if (url === '/') {
         res.write('<html>');
@@ -174,7 +182,8 @@ const server = http.createServer((req, res) => {
         //the if statement will also exit out of this function
     }
 
-    //form action, a url where the generated request should be sent to
+    //form action, a url where 
+    //the generated request should be sent to
     //a button type submit in a form element
     //will send a new request
     //this will automatically target the host it is running on
@@ -193,7 +202,7 @@ const server = http.createServer((req, res) => {
     //POST: has to be created by you by such a form, also other js ways available
 
 
-    
+
     //this code will run because the form will switch to
     // /message, which is not in the if statement
     res.setHeader('Content-Type', 'text/html');
@@ -210,6 +219,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000);
 
+*/
 
 //for /nothing
 //load a page where user enter some date then store in a 
@@ -221,3 +231,189 @@ server.listen(3000);
 //send a response of html
 //gives the user input form and a button to send a new request in return
 //and that would not be a get request
+
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+//Day3
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+//page redirection
+//file creating with the user input provided
+
+
+
+
+const http = require('http');
+const fs = require("fs");   //
+
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    const method = req.method;  //
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+
+    //enter this state only if in /message with method post
+    if (url === "/message" && method === "POST") {   //
+        //the below functions will be registered internally
+        //in njs event emitter registry
+        //and not run right away
+        //then goes to line 358
+
+
+        //receive input(chunk) and output to a txt file
+        //on method, allows us to listen to certain events
+        //listen to the data event
+        //the data event will be fired
+        //whenever a new chunk is ready to be read
+        //with the help of the buffer
+        //2nd parameter, function should be executed
+        //for every data event or data piece
+        //the request body should be an empty array
+        //push a new element onto the body
+        //we cannot change a const,
+        //but with push we are changing the object behind
+        //that data element, not the object itself
+        const requestBody = [];
+        req.on("data", (chunk)=>{
+            console.log(chunk);
+            requestBody.push(chunk);
+        });
+
+        //end event listener
+        //fired once finished parsing the incoming request / request data
+        //Buffer object available globally by NJS
+        //create a buffer and add all the chunks to it
+        //now its like the bus is waiting
+        //toString to convert it to a string
+        //as we know we will enter a text here
+        //different input types different ways to use the buffer
+        return req.on("end", ()=>{
+            const parsedBody = Buffer.concat(requestBody).toString();
+            //console.log(parsedBody);
+            //take the element and split it on the equal sign
+            //then take the element on the index 1
+            //the second element in the resulting array 
+            //the element on the right of the equal sign
+            const message = parsedBody.split("=")[1];
+
+            
+            //fs.writeFileSync("message.txt", message); //
+            //writeFileSync will pause/block code execution
+            //until this file is created
+            //syncronous code
+            //in a huge file blocking will cause
+            //the remaining code not run and 
+            //other requests not be handled
+            //until this file operation is done
+
+            //so use writeFile
+            //which take a third argument
+            //a function that should be executed
+            //once its done
+            //receives error, will be null if no error
+            //if there is, can handle it
+            //by returning a error response or success response
+            fs.writeFile("message.txt", message, (err) => {
+                //this response only be sent
+                //if done working with the file
+                res.statusCode = 302;
+                res.setHeader("Location", "/");
+                return res.end();
+            }); //
+            //this is why nodejs is high performance
+            //because it never stops/blocks the code or server
+
+        });
+        //the above function does not execute immediately
+        //it get registered and the code below continues
+        //so any code dependent on the parsed data
+        //should be moved above like writefile
+        //async function
+        //nodejs adds an event listener internally
+        //for the end event of the request
+        //which will be triggered when nodejs
+        //finishes parsing the request 
+        //and then call the arrow function for you once its done
+        //nodejs internal registers of events
+        //and listeners to these events
+        //and an arrow function like this is such a listener
+        //while AF is executing, njs will not pause the other
+        //code from executing
+        //the AF is a callback
+
+
+        //now you will se in terminal
+        //the chunks
+        //the form-key of the input named message
+        //of our data value (key value pair)
+
+
+        //creating a file
+        //fs.writeFileSync("message.txt", "Dummy text"); //
+
+
+
+
+        //////write meta information for page redirection
+        //to stay on the / page
+        //302 stands for "redirection"
+        //js object with some headers we want to set
+        //"Location", default header accepted by the browser
+        //set the location to /
+        //stop after
+
+        //res.writeHead(302, {}); or these two lines
+        //res.statusCode = 302;
+        //res.setHeader("Location", "/");
+        //return res.end();
+    }
+
+        //now will have in networkDev
+        //message url of status 302 "redirected"
+        //and message.txt file created
+
+        res.setHeader('Content-Type', 'text/html'); //358
+        res.write('<html>');
+        res.write('<head><title>Page1</title></head>');
+        res.write('<body><h1> Hi </h1> </body>');
+        res.write('</html>');
+        res.end();
+
+
+});
+
+
+server.listen(3000);
+
+
+//we want to return the user to "/"
+//and store the use input message in a file
+
+//the incoming data is sent as a stream of data
+//Streams & buffers
+//example incoming request
+//the request is read by Node in chucks/multiple parts
+//till fully parsed
+//so we can work on request received chunks until all received
+//that helps when writing data to the hard drive, while the data is coming in
+
+//a buffer is used to work with chunks
+//hold multiple chunks and work with them
+//before they are released once its done
+
+
+////////////////////////////////////////////////////
+
+//the order of execution of the code
+//not necessarily in the order of writing
+
