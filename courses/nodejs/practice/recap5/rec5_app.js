5e13cc3693b5ab90d528c13c3ba82acf926b725e
SheriffKoder <sheriff.koder@hotmail.com>
2023-06-25 19:01:44 +0300

    Node.js: Express : MVC : recap on section 7 : shop website

---
+++ courses/nodejs/practice/recap5/rec5_app.js
@@ -0,0 +1,80 @@
+/*
+a shop website using file data storage
+
+
+
+*/
+
+
+//(1) default requires
+const http = require("http");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//(2) use body-parser && set static folder
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: false}));
+
+//used the __dirname directly here because we are in a root file
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+//(3) set temp engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+
+//(4) import routes
+const adminJsRoutes = require("./routes/admin.js");
+const shopJsRoutes = require("./routes/shop.js");
+app.use(shopJsRoutes); //replaced code
+//filtering mechanism
+//not put /admin/.. in the routes links but put in the navigation/form etc.
+app.use("/admin", adminJsRoutes);
+
+
+//(5) 404
+const errorController = require("./controllers/errorController.js");
+app.use(errorController.get404);
+
+//(6) listen
+app.listen(3000);
+
+
+/*
+
+///////////////////////////////////////////////////////////////////
+////Preparing the html's
+
+//make admin folder in the views
+put in it the add-product.ejs, edit-product.ejs, products.ejs
+
+
+//shop folder for customers
+put in it the shop.ejs and rename it to product-list.ejs
+adjust the links using this file in other js files and the includes to ../includes
+
+make a starting page index.ejs, product-details.ejs, cart.ejs, checkout
+
+
+//work on the navigation
+add links to all important views
+
+
+///////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
