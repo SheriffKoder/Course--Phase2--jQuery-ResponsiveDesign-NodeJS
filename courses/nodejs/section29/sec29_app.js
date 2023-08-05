57a05fc32c38bf9e20c2ba10d196af39a2efb0d7
SheriffKoder <sheriff.koder@hotmail.com>
2023-08-05 06:05:18 +0300

    Node.js: Express : MVC : NoSQL: MongoDB: Mongoose: Sessions and cookies: Async Requests: deployment: compress js/css assets with expressJS compression, strore request logs in a file using morgan, setup a self-created ssl certificate, sign-up to a hosting provider

--- courses/nodejs/section29/sec29_app.js
+++ courses/nodejs/section29/sec29_app.js
@@ -13,7 +13,10 @@
 //# npm install --save multer //s20: parse incoming files (upload)
 //# npm install --save pdfkit //s20.2 generate pdf files
 //# npm install --save strp //s23 using strp
-//# npm install --save helmet //(29.0.2)
+//# npm install --save helmet //(29.0.2) securing response headers
+//# npm install --save compression //(29.0.3)
+//# npm install --save morgan   //(29.0.4)
+//--# openssl req -nodes -new -x509 -keyout server.key -out server.cert //(29.0.6) setup self ssl
 
 
 const http = require("http");
@@ -28,9 +31,12 @@ const session = require("express-session"); //(2.6)
 const csrf = require("csurf");  //(3.7)
 const flash = require("connect-flash");   //(3.9)
 
-//(29.0.2)
-const helmet = require("helmet");
 
+const helmet = require("helmet"); //(29.0.2)
+const compression = require("compression")//(29.0.3)
+const morgan = require("morgan"); //(29.0.4)
+//const fs = require("fs"); //(29.0.4) to store request logs
+//const https = require("https"); //(29.0.6) allows to spin up https server
 
 const multer = require("multer"); //(20.0.1)
 //diskStorage is a storage engine which can use with multer
@@ -141,8 +147,25 @@ const csrfProtection = csrf();
 app.use(flash());
 
 
-//(29.0.2)
-app.use(helmet());
+app.use(helmet());          //(29.0.2)
+app.use(compression());     //(29.0.2)
+//pass the information how to log
+//write to access.log file
+//flag: "a" means append new data and not overwrite existing
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), {flags: "a"});
+//more info in the official docs
+//that defines which data is being logged, how it is formatted
+app.use(morgan("combined", { stream: accessLogStream}));          //(29.0.4)
+
+
+//(29.0.6)
+//read the file synchronously
+//thus will block code execution until file is read
+//but here i do not want to continue with starting the server
+//unless i read that file in
+//const privateKey = fs.readFileSync("server.key");
+//const certificate = fs.readFileSync("server.cert");
+
 
 
 //(2.6)
@@ -386,9 +409,16 @@ mongoose.connect(MongoDbUri)
         //(29.0.1)
         // app.listen(3000);
         //if undefined use 3000, 
-        //will fall to 300 in a local development if its not set by a hosting provider
+        //will fall to 3000 in a local development if its not set by a hosting provider
         //most hosting providers will inject a PORT environment variable
-        app.listen(process.env.PORT || 3000);
+        app.listen(process.env.PORT || 3000); 
+        
+        //(29.0.6)
+        //takes two arguments
+        //1st configure the server, point it at the private key and cert consts
+        //2nd request handler in our case the express application
+        //https.createServer({key: privateKey, cert: certificate },app)
+        //.listen(process.env.PORT || 3000);
 
     })  
     .catch(err => {
@@ -419,23 +449,26 @@ REST and GraphQL
 both types work the same
 have the same Hosting Requirements
 
-start a node server with node filename.js
+start a node server with "node filename.js"
 and use a node framework (e.g Express)
 
 
-//Preparing the Code for Production
+////Preparing the Code for Production
 
-# want to "use Environment Variables"
+1) want to "use Environment Variables"
 to avoid hard coding certain values in code
 like api keys, port numbers, passwords into your code
+[Done]
 
-# also make sure when using third-party services like stripe
+2) also make sure when using third-party services like stripe
 that you "use the Production API keys"
 and not the development testing keys
+[Done]
 
-# "reduce error output details" as little as possible
+3) "reduce error output details" as little as possible
 to avoid exposing insights into our code
 do not send sensitive info to users
+[Done using the process.env.NODE_ENV variable]
 
 in this course we used the default express handler middleware's
 and the default errors and the custom errors
@@ -445,16 +478,17 @@ which are fine
 remove data out of the errors when deploying the application
 or preparing the application for deployment
 
-# regarding the responses your application sends
+4) regarding the responses your application sends
 "set secure response headers"
 some headers do not hurt, prevents the client from doing certain things
+[Done using helmet package]
 
 > will be shown how to easily set these headers
 and find out which headers they are
 as a best practice
 
-these are handled by the hosting provider:
-# "Add asset compression"
+//these are handled by the hosting provider:
+5) "Add asset compression"
 in a typical node application
 can serve some assets (js/css files)
 and there adding compression can be a good idea
@@ -462,17 +496,19 @@ it can reduce response size, time
 as the client has to download less
 and most browsers are able to download compressed/zipped assets
 and unzip them on the fly directly on the browser
+[Done]
 
-
-# "configure logging"
+6) "configure logging"
 so can be aware/stay-up-to-date of what is happening on your server
 since we are not testing the servers but users now do
+[Done]
 
 
-# "Use SSL/TLS"
+7) "Use SSL/TLS"
 encryption of data in transit
 thus far we used Http server, so communication with the server
 was not encrypted
+[Done]
 
 
 
@@ -481,6 +517,9 @@ was not encrypted
 
 //using the injected environment variables
 in package.json that will be used in the whole code files
+
+!! store sensitive/changing data in code in variables
+that are accessed from elsewhere
 
 we will be working with the shop application
 built before the REST module
@@ -537,9 +576,11 @@ when deploying will set the start in package.json to node app.js
 and in the hosting provider dashboard put the variables
 or if no hosting, put in the script of "start" the variables
 
+>> another way than nodemon (as it is not in production)
+>edit the start script in the package.json
 
 >> process.env.NODE_ENV
-will be set by hosting providers to production
+will be set by hosting providers to "production"
 
 when set to production, will reduce the details for errors it throws
 and optimize somethings for deployment
@@ -574,12 +615,267 @@ can open the dev tools and  will find in networks > localhost > headers
 headers set by helmet
 
 
-
+454-460
 ///////////////////////////////////////////////////////////////////
 //(29.0.3)
 //compressing assets
 
+search node compression on google
+will find express js compression middleware package
 
+which make it easy to add compression to your application
+
+# npm install --save compression
+
+>> just require and app.use the compression in app.js
+then you will find in the dev tools the downloaded static files size
+got smaller
+
+a useful feature when the front end of your code
+is loaded with js/css files of large size
+images are not compressed here to make them load fast
+
+some hosting providers have/offer support of compression
+so if the hosting provider does not support it or you are building
+your own server, can use this package
+
+
+///////////////////////////////////////////////////////////////////
+//(29.0.4)
+
+//setting up request logging
+
+# npm install --save morgan
+
+>> require and app.use
+>> create a file to write to and configure that file in the app.use
+to store the logged data instead of seeing data in the console
+
+this functionality can also be provided by the hosting providers
+unless want to set things manually
+
+also there is another way of logging
+https://blog.risingstack.com/node-js-logging-tutorial/
+
+
+///////////////////////////////////////////////////////////////////
+//(29.0.5)
+
+//SSL/TLS Encryption
+
+TLS is the newer version of SSL
+both are about securing data that is sent from a client to the server
+
+there can be an attacker that Eavesdrop on the data sent
+between the client and server
+
+when the data is in transit SSL/TSL makes it un-readable
+and it will be decrypted on the server
+
+to be able to decrypt it, we will work with a 
+Public/Private key pair
+both keys are known to the server
+
+
+
+
+
+"the private key" 
+will only be known by the server
+because will later be important for decrypting the data
+
+
+"the public key" 
+is not something we have to protect
+will be used for encrypting
+in a SSL certificate we bind this key to a server Identity
+the server Identity is something like the domain, admin email address
+you set that data when you create a certificate
+and send that Public key to the client/browser
+through the SSL certificate
+the client also knows that public key associated with the server
+so the client can encrypt the data which it sends to the server
+and the server will decrypt the data with the private key
+and only that private key can decrypt the data
+
+
+
+
+
+you will use a certificate authority for that
+you can create your own ssl certificates too
+but when you create your own keys
+the browser then does not trust you that the information there is correct
+
+hence in production you will use a SSL cert
+that is provided by a known SSL authority
+which the browser trusts
+to have a real secure and trusted protection
+
+!! public key with server identity sent to user
+when an encrypted request received it is paired with 
+the private key to expose data on the server (safe place)
+
+
+
+
+///////////////////////////////////////////////////////////////////
+//(29.0.6)
+
+//setup a SSL connection on our server
+with a self created certificate
+
+get files
+import https and store files
+read files server.listen
+
+
+can do that with this console command
+which is available on mac/linux by default - windows not default
+for windows search for openssl windows > binaries openSSLwiki > first link (proweb) in description column > download a version and install
+
+//this will give the public/private keys packaged in certificate files
+>> # openssl req -nodes -new -x509 -keyout server.key -out server.cert
+
+will ask some questions
+to connect your identity of your application to your public key
+set common name to "localhost" or the hosted example.com
+
+!! for production should not use the self-created certs
+request the certificate for your domain by some authority
+and they will do this for you
+let the hosting provider set this up
+and the hosting or SP uses SSL
+in the traffic between your app and in between servers does use http
+because its blocked or not available to the public anyways
+the hosting provider's front servers will implement this logic
+so wont write this code on your own
+
+> this will give two files in your folder
+server.cert / server.key(private key)
+
+the private key will always stay on your server
+the server.cert is what we send to the client in the end
+
+
+//in app.js
+>> import the https module, 
+>> store the two files in a constant using fs.readFileSync, 
+
+>> go to the app.listen and use https.createServer 
+to setup an https server with some configurations
+
+!! now the server will start with ssl encryption
+
+>> # npm start
+>> change the url to https://
+
+it will give a connection is not private error in the browser
+but we can still advance
+
+
+>> now reverse back to the normal http and remove ssl
+as will use a service provider's ssl better
+
+
+///////////////////////////////////////////////////////////////////
+//(29.0.7)
+
+//using a hosting provider
+like heruko or AWS
+
+we deploy the code onto managed-spaces/virtual-servers
+on the hosting provider's computers
+where they have powerful machines in their servers
+and we rent part on that machine's resources
+your app runs separated from other user's on these machines
+now want to connect your app on this virtual server with your users
+typically you do not directly connect your space on that machine to your servers
+(though that is also possible with some providers)
+
+the HP give you their own managed server in front of your virtual-server
+where you can conveniently add SSL encryption 
+compression, logging or load balancing
+which means when you have multiple virtual servers
+when your app is doing really well and need more resources
+all that is managed by managed servers
+which are typically invisible to you
+which you do not configure
+which are part of the hosting provider package
+and you just use a nice user interface
+to setup how your app behaves regarding SSL, logging etc.
+
+the virtual server and the managed servers
+run on a private network
+which means that your own virtual server and your code
+are not directly exposed to the web
+but exposed to the managed server
+that in turn talks to the web
+and thus to the users
+through a public server gateway (a door where requests can come in)
+then forwarded to your server / virtual server
+and the responses are also forwarded
+
+
+private network {virtual-server(code) > managed-server(SSL/..)} [public server gateway] < requests
+
+
+
+
+
+
+we can use "Heroku"
+we can start there for free
+
+>> create account
+>> create new app (sheriffkoder-test-project1)
+
+instead will user render.com
+signed in with gmail
+access to /phase2 github repo
+
+in heroku max used the heroku git option to deploy the code
+////////Git is version control system
+helps with saving and managing your source code
+allows to work with "commits, branches, remote repos"
+
+> commits: snapshots of your code, easily switch between commits
+
+> branches: allows to have different version of your code
+allows to not have only one history of your snapshots 
+but multiple histories for different versions of your code
+could have a master branch (production ready), fix bugs add new features
+in other branches like development and new-feature
+
+once other branches ready
+can do merging to merge the new feature branch
+with your main branch so that you have one branch 
+which you could put back into production again
+but it allows you to work on different features
+and different branches without affecting your main finished code for now
+
+this allows to separate the development workflow 
+from your new feature or bug fixing
+
+> Remote Repositories
+means your code is not only stored locally
+as it is by default
+but can store code + commit + branches in the cloud
+
+this means can protect from loss of data
+and access your source code from different machines
+and share it with other developers
+
+and can use this feature to deploy your code automatically
+
+will use heroku GIT as a remote repo 
+when push code to that remote repo to heroku
+it will then be taken by heroku and put into production 
+and the server will be spun up based on it automatically
+
+//install GIT
+>> google for GIT
+>> downloads, download for your OS
 
 
 
